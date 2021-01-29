fbbd06e5a7edd3576f84c15b6ffc05c01ae3f852
Fumasnor <wudehalno3@gmail.com>
2021-01-29 14:44:41 +0300

    edited proxy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -2,7 +2,7 @@ import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client'
 import {setContext} from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000/'
+    uri: "https://boiling-taiga-89004.herokuapp.com/"
 })
 
 
